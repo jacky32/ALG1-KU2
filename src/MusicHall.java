28589811bc0e646d76316af86b943849c85e4ccb
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-19 12:44:18 +0200

    Add constructor

--- src/MusicHall.java
+++ src/MusicHall.java
@@ -7,5 +7,13 @@ public class MusicHall {
   private Sector sector6;
   private Sector sector7;
 
-
+  public MusicHall(Sector s1, Sector s2, Sector s3, Sector s4, Sector s5, Sector s6, Sector s7) {
+    this.sector1 = s1;
+    this.sector2 = s2;
+    this.sector3 = s3;
+    this.sector4 = s4;
+    this.sector5 = s5;
+    this.sector6 = s6;
+    this.sector7 = s7;
+  }
 }
