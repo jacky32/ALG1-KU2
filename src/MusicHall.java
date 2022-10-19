723e535aafbd326c32612682cbbd67a0609c1b65
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-19 12:42:58 +0200

    Create MusicHall class with private attributes

---
+++ src/MusicHall.java
@@ -0,0 +1,11 @@
+public class MusicHall {
+  private Sector sector1;
+  private Sector sector2;
+  private Sector sector3;
+  private Sector sector4;
+  private Sector sector5;
+  private Sector sector6;
+  private Sector sector7;
+
+
+}
