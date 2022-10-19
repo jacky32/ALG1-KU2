b932b543638c5a7ed3301af3f7935bd029265888
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-19 12:33:14 +0200

    Add constructor

--- src/Sector.java
+++ src/Sector.java
@@ -3,4 +3,11 @@ public class Sector {
   private int seatingPlaces;
   private String nameOfSector;
   private String colorOfSector;
+
+  public Sector(String name, int standingPlaces, int seatingPlaces, String colorOfSector) {
+    this.nameOfSector = name;
+    this.standingPlaces = standingPlaces;
+    this.seatingPlaces = seatingPlaces;
+    this.colorOfSector = colorOfSector;
+  }
 }
