b2a4c5c9c2dc2585a1a4dc73a0c49688f28c7135
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-19 12:57:41 +0200

    Refactor variables into HashMap

--- src/MusicHall.java
+++ src/MusicHall.java
@@ -1,19 +1,14 @@
-public class MusicHall {
-  private Sector sector1;
-  private Sector sector2;
-  private Sector sector3;
-  private Sector sector4;
-  private Sector sector5;
-  private Sector sector6;
-  private Sector sector7;
+import java.util.HashSet;
 
+public class MusicHall {
+  private HashSet<Sector> sectors = new HashSet<Sector>();
   public MusicHall(Sector s1, Sector s2, Sector s3, Sector s4, Sector s5, Sector s6, Sector s7) {
-    this.sector1 = s1;
-    this.sector2 = s2;
-    this.sector3 = s3;
-    this.sector4 = s4;
-    this.sector5 = s5;
-    this.sector6 = s6;
-    this.sector7 = s7;
+    this.sectors.add(s1);
+    this.sectors.add(s2);
+    this.sectors.add(s3);
+    this.sectors.add(s4);
+    this.sectors.add(s5);
+    this.sectors.add(s6);
+    this.sectors.add(s7);
   }
 }
